9ead176210e69414bf41e47bfa9cf4be3f08e9ee
Sergei Korchemkin <hellk.pub@gmail.com>
2019-09-04 20:12:05 +0300

    Updates

---
+++ precache-manifest.a122913cf6230cd858890ace473e9339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a529161659858b1bb91969b9dc961cde",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "bf3adeee0dea8a786805",
+    "url": "/color-picker/static/css/main.c8084e57.chunk.css"
+  },
+  {
+    "revision": "7754634b82980058f50a",
+    "url": "/color-picker/static/js/2.da41f0cd.chunk.js"
+  },
+  {
+    "revision": "bf3adeee0dea8a786805",
+    "url": "/color-picker/static/js/main.fcede5a7.chunk.js"
+  },
+  {
+    "revision": "fd86e74ea6ee3c43f9ec",
+    "url": "/color-picker/static/js/runtime~main.7413dd3d.js"
+  }
+]);
